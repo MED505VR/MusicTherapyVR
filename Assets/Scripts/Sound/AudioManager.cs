a60d0154c4c750669480dd409655ffd03ff8b4b1
CodeZage <hectorthoegersen@gmail.com>
2021-09-30 14:34:58 +0200

    Deletede alot of files and changed the big drum to a group drum

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -2,32 +2,47 @@
 using UnityEngine;
 using System;
 
-
-public class AudioManager : MonoBehaviour
+namespace Sound
 {
-    public Sound[] sounds;
-
-    private void Awake()
+    [System.Serializable]
+    public class Sound
     {
-        foreach (var s in sounds)
+        public string name;
+        public AudioClip clip;
+        [Range(0f, 10f)] public float volume;
+        [Range(1f, 2f)] public float pitch;
+
+        public bool loop;
+
+        [HideInInspector] public AudioSource source;
+    }
+
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] sounds;
+
+        private void Awake()
         {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
+            foreach (var s in sounds)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+                s.source.clip = s.clip;
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+                s.source.loop = s.loop;
+            }
         }
-    }
 
-    public void Play(string name)
-    {
-        var s = Array.Find(sounds, Sound => Sound.name == name);
-        s.source.Play();
-    }
+        public void Play(string name)
+        {
+            var s = Array.Find(sounds, Sound => Sound.name == name);
+            s.source.Play();
+        }
 
-    public void Stop(string name)
-    {
-        var s = Array.Find(sounds, Sound => Sound.name == name);
-        s.source.Stop();
+        public void Stop(string name)
+        {
+            var s = Array.Find(sounds, Sound => Sound.name == name);
+            s.source.Stop();
+        }
     }
 }
