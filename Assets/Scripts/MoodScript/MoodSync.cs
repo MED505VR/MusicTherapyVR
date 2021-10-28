13985f4751af095afbcfbd17cb396860677f26e4
ME-Lab SiderumsPC <andalaca@create.aau.dk>
2021-10-28 15:22:44 +0200

    testSync

--- Assets/Scripts/MoodScript/MoodSync.cs
+++ Assets/Scripts/MoodScript/MoodSync.cs
@@ -7,13 +7,13 @@ public class MoodSync : RealtimeComponent<MoodSyncModel>
     //private MeshRenderer _meshRenderer;
     private MoodSound _play;
     private MeshRenderer _meshRenderer;
-    //private MeshRenderer _lightRenderer;
+    private MeshRenderer _lightRenderer;
 
     private void Awake()
     {
         _play = GetComponent<MoodSound>();
         _meshRenderer = GetComponent<MeshRenderer>();
-        //_lightRenderer = GetComponent<MeshRenderer>();
+        _lightRenderer = GetComponent<MeshRenderer>();
     }
 
     protected override void OnRealtimeModelReplaced(MoodSyncModel previousModel, MoodSyncModel currentModel)
@@ -23,7 +23,7 @@ public class MoodSync : RealtimeComponent<MoodSyncModel>
             // Unregister from events
             previousModel.playDidChange -= PlayDidChange;
             previousModel.colorDidChange -= ColorDidChange;
-            //previousModel.lightDidChange -= LightDidChange;
+            previousModel.lightDidChange -= LightDidChange;
         }
 
         if (currentModel != null)
@@ -32,15 +32,15 @@ public class MoodSync : RealtimeComponent<MoodSyncModel>
             if (currentModel.isFreshModel)
                 currentModel.play = _play.play;
                 currentModel.color = _meshRenderer.material.color;
-                //currentModel.color = _lightRenderer.material.color;
+                currentModel.color = _lightRenderer.material.color;
 
             UpdateLoopPlay();
             UpdateMeshRendererColor();
-            //UpdateMeshRendererLight();
+            UpdateMeshRendererLight();
 
             currentModel.colorDidChange += ColorDidChange;
             currentModel.playDidChange += PlayDidChange;
-            //currentModel.lightDidChange += LightDidChange;
+            currentModel.lightDidChange += LightDidChange;
         }
     }
 
@@ -55,10 +55,10 @@ public class MoodSync : RealtimeComponent<MoodSyncModel>
         UpdateMeshRendererColor();
     }
 
-    /*private void LightDidChange(MoodSyncModel model, Color value)
+    private void LightDidChange(MoodSyncModel model, Color value)
     {
         UpdateMeshRendererLight();
-    }*/
+    }
 
     private void UpdateLoopPlay()
     {
@@ -70,10 +70,10 @@ public class MoodSync : RealtimeComponent<MoodSyncModel>
         _meshRenderer.material.color = model.color;
     }
 
-    /*private void UpdateMeshRendererLight()
+    private void UpdateMeshRendererLight()
     {
         _lightRenderer.material.color = model.color;
-    }*/
+    }
 
     public void SetPlay(bool play)
     {
