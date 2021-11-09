b2285566f99ad6f2b38218df974b3b9557e4febc
ME-Lab SiderumsPC <andalaca@create.aau.dk>
2021-11-09 16:51:35 +0100

    Working on stopButton script

---
+++ Assets/Scripts/EmojiWall/stopButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class stopButton : MonoBehaviour
+{
+    [SerializeField]
+    private MoodSync _moodSync;
+}
