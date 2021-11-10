5cec2b138249659f08c1ca84110316d7e7d1092a
Jacob Noer <jnoer19@student.aau.dk>
2021-11-10 11:08:19 +0100

    Xylophone added to main

---
+++ Assets/Scripts/TrackSpeed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackSpeed : MonoBehaviour
+{
+
+    private Vector3 lastPosition; //Here we will store the last position
+    public float speed; //The speed is what we will set the volume with a max of 1 (Volume cant be more than 1)
+
+
+    private void Start()
+    {
+        lastPosition = transform.position; //We take the position from where we spawn and put it to lastPosition 
+    }
+    private void FixedUpdate()  //Instaed of Update() that runs on every frame FixedUpdate runs every 0.2 sec
+    {
+        speed = ((((transform.position - lastPosition).magnitude) / Time.deltaTime) / 2); //How much have we moved in pr sec
+        lastPosition = transform.position; //set the current position to the lastPosition
+    }
+}
---
+++ Assets/Xylo.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class Xylo : MonoBehaviour
+{
+    private AudioSource _source;
+    private RealtimeView myView;
+    [SerializeField]
+    public bool _play;
+    private bool _prevPlay;
+
+    private DrumSync _drumSync;
+
+
+    private void Start()
+    {
+        _drumSync = GetComponent<DrumSync>();
+        myView = GetComponent<RealtimeView>();
+        _source = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _play = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myView.RequestOwnership();
+        if (other.CompareTag("DrumstickLeft") || other.CompareTag("DrumstickRight"))
+        {
+            _play = !_play;
+            _source.volume = other.gameObject.GetComponent<TrackSpeed>().speed; //will set the volume to speed but max 1
+            //color();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        myView.ClearOwnership();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_play != _prevPlay)
+        {
+            _drumSync.SetPlay(_play);
+            _prevPlay = _play;
+        }
+    }
+}
