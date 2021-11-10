c32f3911e634e6327705b4e6ba588871c1cccc45
ME-Lab SiderumsPC <andalaca@create.aau.dk>
2021-11-10 11:19:14 +0100

    button object removed

--- Assets/Scripts/EmojiWall/stopButton.cs
+++ Assets/Scripts/EmojiWall/stopButton.cs
@@ -2,9 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Normal.Realtime;
-
+/*
 public class stopButton : MonoBehaviour
 {
     [SerializeField]
-    private MoodSync _moodSync;
+    private MoodSound _moodSound;
+    private MoodPlay _moodPlay;
+
+    private MeshRenderer _meshRenderer;
+    private RealtimeView myView;
+
+    private GameObject leftHand, rightHand, myObject;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        leftHand = GameObject.Find("LeftHandAnchor");
+        rightHand = GameObject.Find("RightHandAnchor");
+        myView = GetComponent<RealtimeView>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == leftHand || other.gameObject == rightHand)
+        {
+            myView.RequestOwnership();
+            //_moodSound.play = false;
+            _moodPlay._play = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        myView.ClearOwnership();
+    }
 }
+*/
