4b9b3d52b8994312e601742b0e9ea452808632f7
Snaylo <scottjamesnaylor@gmail.com>
2021-11-11 09:28:03 +0100

    movement instrument

--- Assets/Scripts/movement instrument/MovementInstrumentSync.cs
+++ Assets/Scripts/movement instrument/MovementInstrumentSync.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Normal.Realtime;
 
-public class MovementInstrumentSync : MonoBehaviour
+public class MovementInstrumentSync : RealtimeComponent<MovModel>
 {
-    private MovementInstrumentSound _movPlay;
+    private MovementInstrumentSound movement;
 
     private void Start()
     {
-        _movPlay = GetComponent<MovementInstrumentSound>();
+        movement = GetComponent<MovementInstrumentSound>();
     }
     protected override void OnRealtimeModelReplaced(MovModel previousModel, MovModel currentModel)
     {
@@ -24,9 +24,15 @@ public class MovementInstrumentSync : MonoBehaviour
         {
             // If this is a model that has no data set on it, populate it with the current mesh renderer color.
             if (currentModel.isFreshModel)
-                currentModel._MovPlay = _movPlay.play;
-
+                currentModel.movPlay = movement.MovPlay;
+            UpdatePlay();
             currentModel.movPlayDidChange += movPlayDidChange;
         }
+    }
+    private void movPlayDidChange(MovModel model, bool value) {
+        UpdatePlay();
+    }
+    private void UpdatePlay() {
+        movement.MovPlay = MovModel.movPlay;
     }
 }
