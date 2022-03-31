87242ba8363496398c5d4890616148f744199f9a
Jacob Noer <jnoer19@student.aau.dk>
2022-03-31 10:20:11 +0200

    fixed refference error on CSVManager

--- Assets/Scripts/DataRecording/CSVManager.cs
+++ Assets/Scripts/DataRecording/CSVManager.cs
@@ -1,81 +1,75 @@
-using System;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace DataRecording
 {
-    public class CsvManager : MonoBehaviour
+    public class CSVManager : MonoBehaviour
     {
 
-        private DataRecorder dataRecorder;
+        private DataRecorder _dataRecorder;
 
 
         private void Start()
         {
-            dataRecorder = FindObjectOfType<DataRecorder>();
+            _dataRecorder = FindObjectOfType<DataRecorder>();
         }
-        
-        
-       
 
         void FixedUpdate()
         {
 
-            print(dataRecorder.CurrentConnectedPlayers.Count);
-            if (dataRecorder.CurrentConnectedPlayers.Count >= 0)
+            print(_dataRecorder.CurrentConnectedPlayers.Count);
+            if (_dataRecorder.CurrentConnectedPlayers.Count >= 0)
             {
                 CsvPositionWriter.AppendToReport(1,
                     new string[18]
                     {
-                        dataRecorder.CurrentConnectedPlayers[0].head.position.x.ToString(), //float
-                        dataRecorder.CurrentConnectedPlayers[0].head.position.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].head.position.z.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].head.rotation.x.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].head.rotation.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].head.rotation.z.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.position.x.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.position.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.position.z.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.x.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.z.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.position.x.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.position.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.position.z.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.x.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.y.ToString(), //float 
-                        dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].head.position.x.ToString(), //float
+                        _dataRecorder.CurrentConnectedPlayers[0].head.position.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].head.position.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].head.rotation.x.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].head.rotation.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].head.rotation.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.position.x.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.position.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.position.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.x.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].leftHand.rotation.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.position.x.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.position.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.position.z.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.x.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.y.ToString(), //float 
+                        _dataRecorder.CurrentConnectedPlayers[0].rightHand.rotation.z.ToString(), //float 
                     }
                 );
 
-                if (dataRecorder.CurrentConnectedPlayers.Count > 1)
+                if (_dataRecorder.CurrentConnectedPlayers.Count > 1)
                 {
                     CsvPositionWriter.AppendToReport(2,
                         new string[18]
                         {
-                            dataRecorder.CurrentConnectedPlayers[1].head.position.x.ToString(), //float
-                            dataRecorder.CurrentConnectedPlayers[1].head.position.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].head.position.z.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].head.rotation.x.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].head.rotation.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].head.rotation.z.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.position.x.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.position.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.position.z.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.x.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.z.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.position.x.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.position.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.position.z.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.x.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.y.ToString(), //float 
-                            dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].head.position.x.ToString(), //float
+                            _dataRecorder.CurrentConnectedPlayers[1].head.position.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].head.position.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].head.rotation.x.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].head.rotation.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].head.rotation.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.position.x.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.position.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.position.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.x.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].leftHand.rotation.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.position.x.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.position.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.position.z.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.x.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.y.ToString(), //float 
+                            _dataRecorder.CurrentConnectedPlayers[1].rightHand.rotation.z.ToString(), //float 
                         }
                     );
                 }
-                Debug.Log("Report updated succesfully!");
+                Debug.Log("Report updated successfully!");
             }
         }
     }
