87742b8591d6ad418b65413e5a5da181c26d87ba
Jacob Noer <jnoer19@student.aau.dk>
2022-03-31 13:24:49 +0200

    clean up

---
+++ Assets/Scripts/Player/MouthMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class MouthMove : MonoBehaviour
+{
+    public Transform mouth;
+
+    private RealtimeAvatarVoice _voice;
+    private float _mouthSize;
+
+    private void Awake()
+    {
+        // Get a reference to the RealtimeAvatarVoice component
+        _voice = GetComponent<RealtimeAvatarVoice>();
+    }
+
+    private void Update()
+    {
+        // Use the current voice volume (a value between 0 - 1) 
+        // to calculate the target mouth size (between 0.1 and 1.0)
+        var targetMouthSize = Mathf.Lerp(0.1f, 1.0f, _voice.voiceVolume);
+
+        // Animate the mouth size towards the target mouth size to keep 
+        // the open / close animation smooth
+        _mouthSize = Mathf.Lerp(_mouthSize, targetMouthSize, 30.0f * Time.deltaTime);
+
+        // Apply the mouth size to the scale of the mouth geometry
+        var localScale = mouth.localScale;
+        localScale.y = _mouthSize;
+        mouth.localScale = localScale;
+    }
+}
